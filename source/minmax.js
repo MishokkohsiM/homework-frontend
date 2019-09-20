14e707120d1d7864e22763945f80b0d311edf63d
MishokkohsiM <mixanasa@mail.ru>
2019-09-20 14:12:18 +0300

    updated

--- source/minmax.js
+++ source/minmax.js
@@ -1,27 +1,22 @@
 'use strict';
 
-const minmax =  function (array) {
-    let re_1 = /[-+]?\d*\.?\d+(e?\-?\d*)/g ;
-    let re_2 = /-?\Infinity/g;
-    let m;
-    let array_of_numbers = [] ;
-    while ((m = re_1.exec(array)) != null) {
-        array_of_numbers.push(m[0]);
+
+
+const minmax = (array) => {
+    const regexp_of_number = /[-+]?\d*\.?\d+(e?\-?\d*)/g ;
+    const regexp_of_infinity = /-?\Infinity/g;
+    let array_of_numbers = array.match(regexp_of_number);
+    const array_of_infinity = array.match(regexp_of_infinity);
+    if (array_of_infinity){
+        if (array_of_numbers) {
+            array_of_numbers = array_of_numbers.concat(array_of_infinity, array_of_numbers)
+        }else {
+            array_of_numbers = array_of_infinity
+        }
     }
-    while ((m = re_2.exec(array)) != null) {
-        array_of_numbers.push(m[0]);
-    }
-    if (!array_of_numbers.length ) {
+    if (!array_of_numbers) {
         return [undefined, undefined];
     }
-    let result = [array_of_numbers[0], array_of_numbers[0]];
-    for (let number of array_of_numbers){
-        if (Number(result[0]) > number){
-            result[0] = number;
-        }
-        if (Number(result[1]) < number){
-            result[1] = number;
-        }
-    }
-    return [Number(result[0]), Number(result[1])];
+    array_of_numbers.sort((a, b) => a - b );
+    return [Number(array_of_numbers[0]), Number(array_of_numbers[array_of_numbers.length - 1])];
 };
