a6c9df10238fcf8b15a0a0b0d642a100db4b6b20
MishokkohsiM <mixanasa@mail.ru>
2019-09-23 19:12:01 +0300

    update minmax

--- source/minmax.js
+++ source/minmax.js
@@ -2,20 +2,16 @@
 
 
 const minmax = (array) => {
-    const array_Of_Numbers = [];
-    const regexp =/\s+?,?;?:?\s*/;
-    const tag_List = array.split(regexp);
-    tag_List.forEach(function (element) {
-        if (isFinite(element) && element !== ''){
-            array_Of_Numbers.push(element);
-        }
-        if (element ==="-Infinity" || element === "Infinity"){
-            array_Of_Numbers.push(Number(element))
+    const rmTrashRegex =/\s+?,?;?:?\s*/;
+    const tagList = array.split(rmTrashRegex);
+    let arrayOfNumbers = tagList.filter((element) => {
+        if (!isNaN(element) ){
+            return element;
         }
     });
-    if (array_Of_Numbers.length === 0) {
+    if (!arrayOfNumbers.length) {
         return [undefined, undefined];
     }
-    return [Math.min(... array_Of_Numbers), Math.max(... array_Of_Numbers)];
+    return [Math.min(... arrayOfNumbers), Math.max(... arrayOfNumbers)];
 };
 
