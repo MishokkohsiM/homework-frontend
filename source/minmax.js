17318249c2b2d8f9ee6f26de664ba5b267663ce9
Resg <sergey@panichev.su>
2019-09-23 20:54:07 +0300

    исправил

--- source/minmax.js
+++ source/minmax.js
@@ -2,13 +2,12 @@
 
 
 const minmax = (array) => {
+    if (!array){
+        return [undefined, undefined];
+    }
     const rmTrashRegex =/\s+?,?;?:?\s*/;
     const tagList = array.split(rmTrashRegex);
-    let arrayOfNumbers = tagList.filter((element) => {
-        if (!isNaN(element) ){
-            return element;
-        }
-    });
+    let arrayOfNumbers = tagList.filter((element) => !isNaN(element));
     if (!arrayOfNumbers.length) {
         return [undefined, undefined];
     }
